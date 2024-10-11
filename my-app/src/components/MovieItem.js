dcc034d91ab0c0f015b83b030766bcd7ee6ca7e7
Fionn McCarthy <g00414386@atu.ie>
2024-10-11 19:49:32 +0000

    Card for stylized list

--- my-app/src/components/MovieItem.js
+++ my-app/src/components/MovieItem.js
@@ -1,22 +1,57 @@
 // necessary inputs
 import { useEffect } from "react";
+import Card from "react-bootstrap/Card";
 
 const MovieItem = (props) => {
 
     // useEffect() functions handle side effects caused by anything
     // that may be outside of scope of the function such as
     // fetching data, subscriptions etc.
+
+    // useEffect() runs after the renderer by default.
+    // If no dependency is provided, it will execute after
+    // every render cycle.
+
+    // If you provide a dependency array as the second argument,
+    // useEffect() will only run when the given argument's
+    // values update.
+
+    // useEffect() can also return a cleanup function, such as:
+
+    /*
+        useEffect(() => {
+        const subscription = someAPICall();
+  
+        return () => {
+            subscription.unsubscribe(); // Cleanup when component unmounts
+        };
+        }, []);
+    */
+
+    // This is useful for resetting state and cleaning up subscription calls.
+
+    
     useEffect(() => {
-        // debug - log movies to console
+        // debug - log movies to console whenever props mount
+        // or update
         console.log("Movies:", props.myMovies);
-      }, []);
+      }, [props.myMovies]);
     
     // return movie information for MovieItem
     return(
-        <div>
-            <h3>{props.myMovie.Title}</h3>
-            <h6>{props.myMovie.Year}</h6>
-            <img src={props.myMovie.Poster} />
+        <div>   
+            {/*card for stylized list*/}
+            <Card>
+                <Card.Header>
+                    {props.myMovie.Title}
+                </Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <img src={props.myMovie.Poster} alt={props.myMovie.Year}/>
+                        <footer>{props.myMovie.Year}</footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
